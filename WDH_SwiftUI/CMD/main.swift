c0671ca8cb14fa4e67e241d9e3ccfe7f0c95660b
Okan Leenen <okan.leenen@syntax-institut.de>
2024-05-06 13:55:06 +0200

    finish

--- WDH_SwiftUI/CMD/main.swift
+++ WDH_SwiftUI/CMD/main.swift
@@ -34,29 +34,98 @@ print(y)
 
 // MARK: - ForEach
 // jedes Element von der Liste ausgeben
+x.forEach{ element in print(element) }
 
+//// Elemente aus der Liste x mit x2 multiplizieren und in einer neuen Liste speichern
+var z: [Int] = []
+x.forEach{ element in z.append(element * 2) }
+print(z)
 
-// Elemente aus der Liste x mit x2 multiplizieren und in einer neuen Liste speichern
+//// Summe aus der Liste berechnen
+var summe = 0
+x.forEach{ element in summe += element }
+print(summe)
 
+//// Elemente filtern mit Modulo
+x.forEach{ element in
+    if element % 2 == 0 {
+        print(element)
+    }
+}
 
-// Summe aus der Liste berechnen
+//// Elemente filtern mit groesser/kleiner als 30
+x.forEach{ element in
+    if element > 30{
+        print(element)
+    }
+}
 
+//// Index und Wert ausgeben
+x.enumerated().forEach{ index, element in
+    print("Index: \(index), Wert: \(element)")
+}
 
-// Elemente filtern mit Modulo
-
-
-// Elemente filtern mit groesser/kleiner
-
-
-// Index und Wert ausgeben
-
+//// alternative schreibweise mit for
+for (index, element) in x.enumerated(){
+    print("Index: \(index), Wert: \(element)")
+}
 
 // Elemente mit bestimmter Bedingung zaehlen
-
-
+var count = 0
+x.forEach{ element in
+    if element < 30 {
+        count += 1
+        //count = count + 1
+    }
+}
+print(count)
 
 // MARK: - Toggles
 // ein einfaches Beispiel
+var isSoundEnabled = true
+
+func toggleSound(){
+    isSoundEnabled.toggle()
+    if isSoundEnabled{
+        print("Sound ist eingeschaltet")
+    } else{
+        print("Sound ist ausgeschaltet")
+    }
+}
+
+toggleSound()
+toggleSound()
 
 // ein Beispiel mit enum
+enum Heizung{
+    case low, mittel, hoch, sehr_hoch
+}
 
+var currentState: Heizung = .low
+
+func funcHeizung(){
+    switch currentState {
+    case .low:
+        currentState = .mittel
+    case .mittel:
+        currentState = .hoch
+    case .hoch:
+        currentState = .sehr_hoch
+    case .sehr_hoch:
+        currentState = .low
+    }
+}
+
+
+
+funcHeizung()
+print(currentState)
+
+funcHeizung()
+print(currentState)
+
+funcHeizung()
+print(currentState)
+
+funcHeizung()
+print(currentState)
--- WDH_SwiftUI/WDH_SwiftUI/ContentView.swift
+++ WDH_SwiftUI/WDH_SwiftUI/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
+    @State private var vorName = ""
+    @State private var nachName = ""
+    @State private var geburtstag = Date()
+    @State private var newsLetter = false
+    @State private var datenSchutz = "https://https://de.wikipedia.org/wiki/Datenschutz"
     
     var body: some View {
-        VStack {
-            Text("Test:")
-                .font(.title)
+        NavigationView{
+            Form{
+                Section(header: Text("Persoenliche Information")){
+                    TextField("Vorname", text: $vorName)
+                    TextField("Nachname", text: $nachName)
+                    DatePicker("Geburtstag", selection: $geburtstag, displayedComponents: .date)
+                } .accentColor(.green)
+                
+                Section(header: Text("Einstellungen")){
+                    Toggle("Newsletter", isOn: $newsLetter)
+                    Link("Datenschutz", destination: URL(string: datenSchutz)!)
+                } .accentColor(.yellow)
+            } .navigationTitle("Konto")
         }
+        
     }
 }
 
